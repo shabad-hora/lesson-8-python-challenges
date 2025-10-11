52f3fa1cd4080b51d8402f549b166214ace341b9
shabad-hora <aashihora20@gmail.com>
2025-10-11 18:43:44 +0100

    Add files via upload

---
+++ operator presadence.py
@@ -0,0 +1,13 @@
+v = 1
+w = 2
+x = 6
+y = 7
+z = 51
+z = (v+w)* x / y;
+print("value of (v+w)* x / y is", z)
+name = "bob"
+age = 24
+if name=="bob" or name=="john" and age >=2:
+    print("welcome")
+else:
+    print("goodbye")   
